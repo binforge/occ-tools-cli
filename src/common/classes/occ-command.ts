0f36ff9b7be43ab841a17b734f3290f0bf2c8d8a
Matesz Piątkowski <mateusz.piatkowski@motorolasolutions.com>
2020-06-28 16:51:37 +0200

    add tslog, node-notifier

--- src/common/classes/occ-command.ts
+++ src/common/classes/occ-command.ts
@@ -1,12 +1,22 @@
-import { Command } from '@oclif/command'
-import * as Config from '@oclif/config'
+import { Command } from '@oclif/command';
+import * as Config from '@oclif/config';
+import { RequestService } from '../services/RequestService';
+import { Logger } from 'tslog';
 
 export default abstract class OccCommand extends Command {
-	protected token: string
+	// protected token: string;
+	private logger = new Logger();
+  private requestService = new RequestService();
 	constructor(argv: string[], config: Config.IConfig) {
-		super(argv, config)
-		// console.log('env', process.env.OCC_AUTH_TEST2);
+		super(argv, config);
+		this.authenticate();
 	}
 
-	private authenticate() {}
+	private async authenticate() {
+		const data = await this.requestService.send({
+			method: 'POST',
+			url: 'https://<subdomain>.<domain>.com:<port>/<resource-path>',
+		});
+		this.logger.info(data);
+	}
 }
