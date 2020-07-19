561dba4621ea5804e2417cb60503e4f05bb45734
Mateusz Piątkowski <garadim12@gmail.com>
2020-07-19 14:32:37 +0200

    moved auth logic to separate files

---
+++ src/common/services/RequestData.ts
@@ -0,0 +1,20 @@
+import * as qs from 'querystring';
+
+const SUBDOMAIN = process.env[`OCC_SUBDOMAIN_${process.env.CURRENT_ENV}`];
+const AUTH_KEY = process.env[`OCC_AUTH_${process.env.CURRENT_ENV}`];
+
+export default {
+	authenticate() {
+		return {
+			method: 'POST',
+			url: `https://ccadmin-${SUBDOMAIN}-${process.env.ORGANIZATION_ID}.oracleoutsourcing.com/ccadmin/v1/login`,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: `Bearer ${AUTH_KEY}`,
+			},
+			data: qs.stringify({
+				grant_type: 'client_credentials',
+			}),
+		};
+	},
+};
